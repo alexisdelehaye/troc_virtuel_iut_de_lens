ae5bccc687ea49ac185335645c67d7cfd7156b9c
Anthony Hacart <anthony.hacart@gmail.com>
2019-01-08 23:35:07 +0100

    Ajout de la partie admin objet (atm uniquement filtre user/cat)

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -11,6 +11,7 @@ use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Faker;
 
 class AppFixtures extends Fixture
 {
@@ -51,6 +52,32 @@ class AppFixtures extends Fixture
             ->setRoles(['ROLE_ADMIN'])
             ->setPassworduser($this->encoder->encodePassword($user2, "secret"));
         $manager->persist($user2);
+
+        $catlivres = new Categorie();
+        $catlivres->setNomcategorie("Livres")
+            ->setDescriptioncategorie("Livre en tout genres");
+        $manager->persist($catlivres);
+
+        $catdvd = new Categorie();
+        $catdvd->setNomcategorie("DVD");
+        $manager->persist($catdvd);
+
+        $arr_cat = [$catlivres, $catdvd];
+        $arr_user = [$user, $user2];
+
+        // On configure dans quelles langues nous voulons nos données
+        $faker = Faker\Factory::create('fr_FR');
+
+        // on créé 10 personnes
+        for ($i = 0; $i < 15; $i++) {
+            $objet = new Objet();
+            $objet->setNomobjet($faker->sentence(3))
+                ->setDescriptionobjet($faker->paragraph())
+                ->setDisponible($faker->randomElement([true,false]))
+                ->setIdcategorie($faker->randomElement($arr_cat))
+                ->setIdproprietaire($faker->randomElement($arr_user));
+            $manager->persist($objet);
+        }
 
         $manager->flush();
 
